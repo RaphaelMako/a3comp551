f79a73160be368ec6564c0b19b2d4292275a8e28
Samuel M <samuel.morris2@mail.mcgill.ca>
2021-11-30 14:04:53 -0500

    d

--- model_definitions.py
+++ model_definitions.py
@@ -11,13 +11,13 @@ class LeNet5(nn.Module):
         self.preprocess = preprocess # Preprocess lambda ;)
         
         self.feature_extractor = nn.Sequential(            
-            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, stride=1),
+            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, stride=1, padding=2),
             nn.Tanh(),
             nn.AvgPool2d(kernel_size=2),
-            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1),
+            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1, padding=2),
             nn.Tanh(),
             nn.AvgPool2d(kernel_size=2),
-            nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5, stride=1),
+            nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5, stride=1, padding=2),
             nn.Tanh()
         )
 
@@ -35,3 +35,24 @@ class LeNet5(nn.Module):
         logits = self.classifier(x)
         probs = F.softmax(logits, dim=1)
         return logits, probs
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
+        self.fc1 = nn.Linear(1936, 968)
+        self.fc2 = nn.Linear(968, 484)
+        self.fc3 = nn.Linear(484, 260)
+
+    def forward(self, x):
+        x = x.view(-1, 1, 56, 56)
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x, F.softmax(x, dim=1)
